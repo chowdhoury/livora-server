5856a8999d3fe9f7edb90ee1c531e0f90afc5344
Raihan <mdr8077@gmail.com>
2025-11-10 05:24:05 +0600

    Properties API Done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to livora server!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to livora server!");
+});
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@chowdhoury.6j2rpnl.mongodb.net/?appName=chowdhoury`;
 const client = new MongoClient(uri, {
@@ -22,22 +22,63 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log(
-          "Pinged your deployment. You successfully connected to MongoDB!"
-        );
-    } finally {
-        
-    }
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const database = client.db("livoraDB");
+    const propertiesCollection = database.collection("properties");
+
+    // Properties API
+    app.get("/api/properties", async (req, res) => {
+      const query = {};
+      if (req.query.email) {
+        query.userEmail = req.query.email;
+      }
+      const cursor = propertiesCollection.find(query);
+      const properties = await cursor.toArray();
+      res.send(properties);
+    });
+
+    app.get("/api/properties/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const property = await propertiesCollection.findOne(query);
+      res.send(property);
+    });
+
+    app.post("/api/properties", async (req, res) => {
+      const newProperty = req.body;
+      const result = await propertiesCollection.insertOne(newProperty);
+      res.send(result);
+    });
+
+    app.patch("/api/properties/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: updatedData,
+      };
+      const result = await propertiesCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/api/properties/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await propertiesCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
-run().catch(console.dir)
-
-
-
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/`);
-})
+  console.log(`Server is running on http://localhost:${port}/`);
+});
