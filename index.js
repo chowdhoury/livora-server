049729668234b5d1d8a1edf1b05e499b94f914dc
Raihan <mdr8077@gmail.com>
2025-11-13 19:48:05 +0600

    APi Updated

--- index.js
+++ index.js
@@ -30,8 +30,27 @@ async function run() {
     );
 
     const database = client.db("livoraDB");
+    const userCollection = database.collection("users");
     const propertiesCollection = database.collection("properties");
     const ratingsCollection = database.collection("ratings");
+
+    app.get("/api/users", async (req, res) => {
+      const query = {};
+      if (req.query.email) {
+        query.email = req.query.email;
+      }
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.post("/api/users",  async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+
 
     // Properties API
     app.get("/api/properties", async (req, res) => {
@@ -39,10 +58,12 @@ async function run() {
       if (req.query.email) {
         query.userEmail = req.query.email;
       }
-      const cursor = propertiesCollection.find(query);
+      const cursor = propertiesCollection.find(query).sort({ createdAt: -1 });
       const properties = await cursor.toArray();
       res.send(properties);
     });
+
+
     app.get("/api/properties/featured", async (req, res) => {
       const query = {};
       const cursor = propertiesCollection.find(query).sort({ createdAt: -1 }).limit(6);
@@ -54,6 +75,12 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const property = await propertiesCollection.findOne(query);
+      const userQuery = { email: property.userEmail };
+      const user = await userCollection.findOne(userQuery);
+      property.userName = user?.name || "Unknown";
+      property.userPhoto = user?.photoURL || "";
+      
+      // console.log('ok',property.userPhoto);
       res.send(property);
     });
 
@@ -83,35 +110,43 @@ async function run() {
 
 
     // Ratings API
-    // app.get("/api/ratings", async (req, res) => {
-    //   const filter = {};
-    //   if(req.query.email){
-    //     filter.reviewerEmail = req.query.email;
-    //   }
-    //   const cursor = ratingsCollection.find(filter);
-    //   const ratings = await cursor.toArray();
-    //   res.send(ratings);
-    // });
+    app.get("/api/ratings", async (req, res) => {
+      const filter = {};
+      if(req.query.email){
+        filter.reviewerEmail = req.query.email;
+      }
+      const cursor = ratingsCollection.find(filter);
+      const ratings = await cursor.toArray();
+      res.send(ratings);
+    });
 
-    // app.get("/api/ratings/user/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //   console.log(email);
-    //   const query = { reviewerEmail: email };
-    //   const cursor = ratingsCollection.find(query);
-    //   const ratings = await cursor.toArray();
-    //   res.send(ratings);
-    // });
+   app.get("/api/ratings/property/:propertyId", async (req, res) => {
+     const propertyId = req.params.propertyId;
+     const userEmail = req.query.email;
 
-    // app.get("/api/ratings/property/:propertyId", async (req, res) => {
-    //   const propertyId = req.params.propertyId;
-    //   const query = { propertyId: propertyId};
-    //   // if(req.query.email){
-    //   //   query.reviewerEmail = req.query.email;
-    //   // }
-    //   const cursor = ratingsCollection.find(query).sort({ createdAt : -1 });
-    //   const ratings = await cursor.toArray();
-    //   res.send(ratings);
-    // });
+     const query = {
+       propertyId: propertyId,
+       userEmail: { $ne: userEmail }, // exclude current user's rating
+     };
+
+     const ratings = await ratingsCollection.find(query).toArray();
+     res.send(ratings);
+   });
+    
+   app.get("/api/myRatings/property/:propertyId", async (req, res) => {
+     const propertyId = req.params.propertyId;
+     const userEmail = req.query.email;
+
+     const query = {
+       propertyId: propertyId,
+       userEmail: userEmail,
+     };
+
+     const result = await ratingsCollection.findOne(query);
+     res.send(result);
+   });
+
+
 
     app.post("/api/ratings", async (req, res) => {
       const newRating = req.body;
@@ -119,23 +154,7 @@ async function run() {
       res.send(result);
     });
 
-    // app.patch("/api/ratings/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatedData = req.body;
-    //   const query = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: updatedData,
-    //   };
-    //   const result = await ratingsCollection.updateOne(query, updateDoc);
-    //   res.send(result);
-    // });
 
-    // app.delete("/api/ratings/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await ratingsCollection.deleteOne(query);
-    //   res.send(result);
-    // });
   } finally {
   }
 }
